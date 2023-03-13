7cf40e2dd67c8c8a42eceaf9c24401a9457532c7
vrrusali <127300744+vrrusali@users.noreply.github.com>
2023-03-13 18:10:19 +0530

    Create Selenium.java

---
+++ src/main/java/com/qa/opencart/pages/Selenium.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class Orderpage {
+
+		By loc= By.id("Selenium");
+		
+		public void getOrder() 
+			{
+			System.out.println("Selenium.java");
+		}
+
+	}
