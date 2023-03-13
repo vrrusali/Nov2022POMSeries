5e83d445992812382669e6ffa05d028ceb60383a
vrrusali <127300744+vrrusali@users.noreply.github.com>
2023-03-13 18:13:56 +0530

    Update Selenium.java

--- src/main/java/com/qa/opencart/pages/Selenium.java
+++ src/main/java/com/qa/opencart/pages/Selenium.java
@@ -2,13 +2,13 @@ package com.qa.opencart.pages;
 
 import org.openqa.selenium.By;
 
-public class Selenium {
+public class xyz {
 
-		By loc= By.id("Selenium");
+		By loc= By.id("xyz");
 		
 		public void getOrder() 
 			{
-			System.out.println("Selenium.java");
+			System.out.println("xyz.java");
 		}
 
 	}
