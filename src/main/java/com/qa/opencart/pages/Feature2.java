dd7bc583a7d7a8045cbe09cfde425a5a32fce8f7
vrrusali <vrrusali@gmail.com>
2023-03-14 15:56:01 +0530

    Updated the Feature files

--- src/main/java/com/qa/opencart/pages/Feature2.java
+++ src/main/java/com/qa/opencart/pages/Feature2.java
@@ -3,7 +3,7 @@ package com.qa.opencart.pages;
 public class Feature2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("F2 feature file for temporary delete");
 
 	}
 
--- src/main/java/com/qa/opencart/pages/Feature3.java
+++ src/main/java/com/qa/opencart/pages/Feature3.java
@@ -3,8 +3,7 @@ package com.qa.opencart.pages;
 public class Feature3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("F3 feature for the Permanent delete");
 	}
 
 }
